866acc5a8da7653b8963a3b8ff74e1e62ef5a0eb
yuniverses <shirojun713@icloud.com>
2022-11-21 00:32:00 +0800

    1.0.8

--- js/canvas.js
+++ js/canvas.js
@@ -46,13 +46,13 @@ function finishedPosition() {
 
 // 繪圖過程
 function draw(e) {
-  // 滑鼠移動過程中，若非繪圖狀態，則跳出
+  e.preventDefault();
   if (!isPainting) return;
 
-  // 取得滑鼠 / 手指在畫布上的 x, y 軸位置位置
+  // 取得滑鼠 / 手指位置
   const paintPosition = getPaintPosition(e);
 
-  // 移動滑鼠位置並產生圖案
+  // 移動到滑鼠位置並產生圖案
   ctx.lineTo(paintPosition.x, paintPosition.y);
   ctx.stroke();
 }
