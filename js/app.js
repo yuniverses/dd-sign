6b272d822974a1b7b4936639d2defa2596280809
yuniverses <shirojun713@icloud.com>
2022-11-21 02:35:03 +0800

    v1.2.2page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 //將其餘畫面隱藏
-(function() {
-    var x = document.getElementById("index");
-    x.style.display = "block";
-    var x = document.getElementById("file-preview");
-    x.style.display = "none";
-    var x = document.getElementById("func-sign");
-    x.style.display = "";
-})()
+  $("#index").show(); 
+  $("#file-preview").hide(); 
+  $("#func-sign").hide(); 
+  $("#put-sign").hide(); 
+// (function() {
+//     var x = document.getElementById("index");
+//     x.style.display = "";
+//     var x = document.getElementById("file-preview");
+//     x.style.display = "none";
+//     var x = document.getElementById("func-sign");
+//     x.style.display = "none";
+// })()
   
 //上傳後從首頁切換到確認頁面（代碼在pdf.js）
-
+$("#save-signature").click(function(){
+  $("#index").hide(); 
+  $("#file-preview").hide(); 
+  $("#func-sign").hide(); 
+  $("#put-sign").show(); 
+});
 
 //確認後從切換到簽名頁面
   function tofuncsign() {
